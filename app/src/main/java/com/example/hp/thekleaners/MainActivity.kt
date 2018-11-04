79483fcb2be053254cd288a99e8368c700856e12
Pawan Kumar <pk9910765616@gmail.com>
2018-11-05 02:07:54 +0530

    Added activities

--- app/src/main/java/com/example/hp/thekleaners/MainActivity.kt
+++ app/src/main/java/com/example/hp/thekleaners/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.hp.thekleaners
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //hvufbttt
+
+        mChooseLanguage.setOnClickListener { onClickListener() }
     }
+
+    private fun onClickListener(){
+        val intent = Intent(this, LanguageChoose::class.java)
+        startActivity(intent)
+    }
+
 }
