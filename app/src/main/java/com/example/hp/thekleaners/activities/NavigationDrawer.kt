bfc6e1aacd30521a2d50c598e633e154ddf0f27d
Pawan Kumar <pk9910765616@gmail.com>
2018-11-09 02:03:59 +0530

    Navigation Items

--- app/src/main/java/com/example/hp/thekleaners/activities/NavigationDrawer.kt
+++ app/src/main/java/com/example/hp/thekleaners/activities/NavigationDrawer.kt
@@ -3,6 +3,7 @@ package com.example.hp.thekleaners.activities
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -77,15 +78,22 @@ class NavigationDrawer : AppCompatActivity(), NavigationView.OnNavigationItemSel
             R.id.nav_camera -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> { }
-            R.id.nav_slideshow -> { }
-            R.id.nav_manage -> { }
-            R.id.nav_share -> { }
-            R.id.nav_send -> { }
+            R.id.mHome -> { }
+            R.id.mLanguage -> { }
+            R.id.mServices -> { }
+            R.id.mHelp -> { }
+            R.id.mShare -> { }
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        if (supportFragmentManager.findFragmentById(R.id.containerView).tag != "FragmentMain")
+            supportFragmentManager.beginTransaction().addToBackStack(null).replace(R.id.containerView, fragment).commit()
+        else
+            supportFragmentManager.beginTransaction().replace(R.id.containerView, fragment).commit()
     }
 
     private fun signInListener() {
