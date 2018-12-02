cc58e52c6c207dc286e5784cc94e9ad45052532f
Pawan Kumar <pk9910765616@gmail.com>
2018-12-02 15:09:19 +0530

    Material Design & profile & password & Address ADD

--- app/src/main/java/com/example/hp/thekleaners/fragments/ChooseServices.kt
+++ app/src/main/java/com/example/hp/thekleaners/fragments/ChooseServices.kt
@@ -7,34 +7,22 @@ import android.view.ViewGroup
 import com.example.hp.thekleaners.R
 import com.example.hp.thekleaners.activities.NavigationDrawer
 import kotlinx.android.synthetic.main.app_bar_navigation_drawer.*
-import kotlinx.android.synthetic.main.fragment_profile.*
 
-class Profile : BaseNavigationFragment() {
+class ChooseServices : BaseNavigationFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_choose_service, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         mainActivity = activity as NavigationDrawer
         mainActivity.toolbar.visibility = View.GONE
         mainActivity.title_name.text = resources.getString(R.string.signIn)
         mainActivity.tabLayout.visibility = View.GONE
         (activity as NavigationDrawer).setDrawerLocked(true)
-        mRelativeLayoutMyAddress.setOnClickListener { mRelativeLayoutMyAddressFunction() }
-        mRelativeLayoutMyService.setOnClickListener { mRelativeLayoutMyServiceFunction() }
-
 
     }
-
-    private fun mRelativeLayoutMyAddressFunction() {
-        fragmentManager!!.beginTransaction().addToBackStack(null).replace(R.id.containerView, SavedAddress()).commit()
-    }
-
-    private fun mRelativeLayoutMyServiceFunction() {
-        fragmentManager!!.beginTransaction().addToBackStack(null).replace(R.id.containerView, ChooseServices()).commit()
-    }
-
 
 }
--- app/src/main/java/com/example/hp/thekleaners/fragments/SavedAddress.kt
+++ app/src/main/java/com/example/hp/thekleaners/fragments/SavedAddress.kt
@@ -7,34 +7,29 @@ import android.view.ViewGroup
 import com.example.hp.thekleaners.R
 import com.example.hp.thekleaners.activities.NavigationDrawer
 import kotlinx.android.synthetic.main.app_bar_navigation_drawer.*
-import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_saved_address.*
 
-class Profile : BaseNavigationFragment() {
+class SavedAddress : BaseNavigationFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_saved_address, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         mainActivity = activity as NavigationDrawer
         mainActivity.toolbar.visibility = View.GONE
         mainActivity.title_name.text = resources.getString(R.string.signIn)
         mainActivity.tabLayout.visibility = View.GONE
         (activity as NavigationDrawer).setDrawerLocked(true)
-        mRelativeLayoutMyAddress.setOnClickListener { mRelativeLayoutMyAddressFunction() }
-        mRelativeLayoutMyService.setOnClickListener { mRelativeLayoutMyServiceFunction() }
+        mSavedNewAddress.setOnClickListener { mSavedNewAddressFunction() }
 
 
     }
 
-    private fun mRelativeLayoutMyAddressFunction() {
-        fragmentManager!!.beginTransaction().addToBackStack(null).replace(R.id.containerView, SavedAddress()).commit()
+    private fun mSavedNewAddressFunction() {
+        fragmentManager!!.beginTransaction().addToBackStack(null).replace(R.id.containerView, AddAddress()).commit()
     }
-
-    private fun mRelativeLayoutMyServiceFunction() {
-        fragmentManager!!.beginTransaction().addToBackStack(null).replace(R.id.containerView, ChooseServices()).commit()
-    }
-
 
 }
