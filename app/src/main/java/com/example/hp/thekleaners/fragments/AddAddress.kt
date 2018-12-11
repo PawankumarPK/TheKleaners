ef238f183bc120413495cd727d66f69427125cbf
Pawan Kumar <pk9910765616@gmail.com>
2018-12-11 14:57:59 +0530

    Material Design & Services ,Others

--- app/src/main/java/com/example/hp/thekleaners/fragments/AddAddress.kt
+++ app/src/main/java/com/example/hp/thekleaners/fragments/AddAddress.kt
@@ -19,8 +19,7 @@ class AddAddress : BaseNavigationFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         mainActivity = activity as NavigationDrawer
-       // mainActivity.toolbar.visibility = View.GONE
-        mainActivity.title_name.text = resources.getString(R.string.signIn)
+        mainActivity.toolbar.visibility = View.VISIBLE
         mainActivity.tabLayout.visibility = View.GONE
         (activity as NavigationDrawer).setDrawerLocked(true)
         mContinueAddAdress.setOnClickListener { mContinueAddAdressFunction() }
