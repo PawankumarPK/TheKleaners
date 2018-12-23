c30faf7c78945b4a8a3d66f9f78a3a51cd10a645
Pawan Kumar <pk9910765616@gmail.com>
2018-12-23 15:51:21 +0530

    Material Design & Add ADDRESS fragment with firebase Working

--- app/src/main/java/com/example/hp/thekleaners/fragments/AddAddress.kt
+++ app/src/main/java/com/example/hp/thekleaners/fragments/AddAddress.kt
@@ -11,6 +11,8 @@ import com.example.hp.thekleaners.R
 import com.example.hp.thekleaners.activities.NavigationDrawer
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
@@ -27,6 +29,7 @@ class AddAddress : BaseNavigationFragment() {
     private var firebaseFirestore: FirebaseFirestore? = null
 
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_add_address, container, false)
     }
@@ -41,8 +44,84 @@ class AddAddress : BaseNavigationFragment() {
         mContinueAddAdress.setOnClickListener { mContinueAddAdressFunction() }
         mAddAddressBackArrow.setOnClickListener { mAddAddressBackArrowFunction() }
 
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseFirestore = FirebaseFirestore.getInstance()
+        user_id = FirebaseAuth.getInstance().uid
+
+        storageReference = FirebaseStorage.getInstance().reference
+
+        addAddress_progress.visibility = View.VISIBLE
+        mContinueAddAdress.isEnabled = false
+
+
+
+        firebaseFirestore!!.collection("Users").document(user_id!!).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+
+                if (task.result.exists()) {
+
+                    val name = task.result.getString("name")
+                    val surname = task.result.getString("surname")
+                    val number = task.result.getString("number")
+
+                    mAddress.setText(name)
+                    mLandmark.setText(surname)
+                    PinCode.setText(number)
+
+                }
+
+            } else {
+
+                val error = task.exception!!.message
+                Toast.makeText(context, "(FIRESTORE Retrieve Error) : $error", Toast.LENGTH_LONG).show()
+
+            }
+            addAddress_progress.visibility = View.INVISIBLE
+            mContinueAddAdress.isEnabled = true
+        }
+
+
+        mContinueAddAdress.setOnClickListener(View.OnClickListener {
+            val user_name = mAddress.text.toString()
+            val surname_name = mLandmark.text.toString()
+            val number_name = PinCode.text.toString()
+            if (!TextUtils.isEmpty(user_name) || !TextUtils.isEmpty(surname_name) || !TextUtils.isEmpty(number_name)) {
+
+                addAddress_progress.visibility = View.VISIBLE
+
+                storeFirestore(null, user_name, surname_name, number_name)
+            }
+        })
+
+
     }
-    private fun mContinueAddAdressFunction() {
+
+
+    private fun storeFirestore(task: Task<UploadTask.TaskSnapshot>?, user_name: String, surname: String, number: String) {
+
+        val userMap = HashMap<String, String>()
+        userMap["name"] = user_name
+        userMap["surname"] = surname
+        userMap["number"] = number
+
+        firebaseFirestore!!.collection("Users").document(user_id!!).collection("Address").add(userMap as Map<String, Any>).addOnCompleteListener { task ->
+
+            if (task.isSuccessful) {
+                Toast.makeText(context, "User Settings are updated", Toast.LENGTH_SHORT).show()
+
+                fragmentManager!!.beginTransaction().addToBackStack(null).replace(R.id.containerView, Profile()).commit()
+
+
+            }
+        }
+
+}
+
+
+
+
+private fun mContinueAddAdressFunction() {
         fragmentManager!!.beginTransaction().addToBackStack(null).replace(R.id.containerView, SavedAddress()).commit()
     }
 
